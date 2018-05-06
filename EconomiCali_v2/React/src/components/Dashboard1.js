6544ccbb53f1a2584075a3b9d54a06241d7bcced
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2018-05-06 16:39:45 -0700

    addde charts

--- EconomiCali_v2/React/src/components/Dashboard1.js
+++ EconomiCali_v2/React/src/components/Dashboard1.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import Navbarmain from "./Navbarmain";
 import {projectdetails} from "../actions";
 import {Doughnut,Bar,Pie} from 'react-chartjs-2';
 import * as API from "../api/API";
+import AuthorByCategoty from "./charts/authorsBillCategory";
+import BillStatusByCategory from "./charts/billStatusByCategory";
+import NavbarTemp from "./NavbarTemp";
 const ReactHighcharts = require('react-highcharts');
+
+
 
 class Dashboard extends Component {
 
@@ -16,28 +20,21 @@ class Dashboard extends Component {
         this.state = {
             userId: '',
             view:"projects",
-            config: {
-            /* HighchartsConfig */
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-            },
-            series: [{
-                data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-            }]
-        },
-            datasets: [
-                {
-                    label: 'My First dataset',
-                    backgroundColor: 'rgba(255,99,132,0.2)',
-                    borderColor: 'rgba(255,99,132,1)',
-                    borderWidth: 1,
-                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                    hoverBorderColor: 'rgba(255,99,132,1)',
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                }
-            ]
+            config:{
+                chart: {
+                    type: 'pie'
+                },
+                /* HighchartsConfig */
+                xAxis: {
+                    categories: []
+                },
+                series: [{
+                    name: 'Tokyo',
+                    data: []
 
-        };
+                }]
+            }
+        }
     }
 
 
@@ -49,7 +46,7 @@ class Dashboard extends Component {
                 (response) =>{
                     console.log(response.data);
 
-                    var res = response.data.filter(function (el) {
+                    var res = response.data.filter(function (el ) {
                         return "Health-care".indexOf(el._id.category) >= 0;
                     });
                     // filter by category
@@ -58,7 +55,7 @@ class Dashboard extends Component {
 
                     var authors = [];
                     var counts= [];
-                    res.map((data) => {
+                    res.slice(0, 5).map((data) => {
                         authors.push(data._id.author);
                         counts.push(data.count);
                     })
@@ -88,32 +85,24 @@ class Dashboard extends Component {
 
 
     render(){
-        var options={
-            legend: {
-                display: true,
-            }
-        }
-        const pieData = [
-            {
-                value: 10,
-                color: "#87BC5E",
-                highlight: "#3C7113",
-                label: "Incoming Funds"
-            },
-            {
-                value: 20,
-                color:"#FF5A5E",
-                highlight: "#D46A6A",
-                label: "Incoming Funds"
-            }
-        ];
         return(
-            <div>
-                <ReactHighcharts config = {this.state.config}/>
-                <Pie
-                    data={pieData}
-                    options={options}
-                />
+            <div className="content-wrapper">
+                <nav className="navbar navbar-default ">
+                </nav>
+                <NavbarTemp/>
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-md-6"><AuthorByCategoty /></div>
+                    <div className="col-md-6"><BillStatusByCategory/></div>
+                </div>
+            </div>
+                <footer className="sticky-footer">
+                    <div className="container">
+                        <div className="text-center">
+                            <small>Copyright © Economicali 2018</small>
+                        </div>
+                    </div>
+                </footer>
             </div>
 
         )
