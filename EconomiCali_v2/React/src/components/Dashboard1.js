de3a159c1a1e5b7a8343b2cb5533b82281141738
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2018-05-09 11:57:32 -0700

    graph change bug fixing

--- EconomiCali_v2/React/src/components/Dashboard1.js
+++ EconomiCali_v2/React/src/components/Dashboard1.js
@@ -33,11 +33,16 @@ class Dashboard extends Component {
                     data: []
 
                 }]
-            }
+            },
+            value:"Education"
         }
+        this.change = this.change.bind(this);
     }
 
-
+    change(event){
+        this.setState({value: event.target.value});
+        console.log(this.state.value);
+    }
 
     render(){
         return(
@@ -58,7 +63,7 @@ class Dashboard extends Component {
             <br/>
             <div className="row">
                     <div className="col-md-2 offset-md-6">
-                    <select id="category" name="category" className="form-control">
+                    <select id="category" name="category" className="form-control" onChange={this.change} >
                         <option value="Education">Education</option>
                         <option value="Health-care">Health-care</option>
                         <option value="Corporation">Corporation</option>
@@ -69,8 +74,8 @@ class Dashboard extends Component {
                 <br/>
                 <br/>
                 <div className="row">
-                    <div className="col-md-6"><AuthorByCategoty /></div>
-                    <div className="col-md-6"><BillStatusByCategory/></div>
+                    <div className="col-md-6"><AuthorByCategoty value={this.state.value}/></div>
+                    <div className="col-md-6"><BillStatusByCategory value={this.state.value}/></div>
                 </div>
             </div>
                 <footer className="sticky-footer">
--- EconomiCali_v2/React/src/components/charts/billStatusByCategory.js
+++ EconomiCali_v2/React/src/components/charts/billStatusByCategory.js
@@ -12,6 +12,7 @@ class BillStatusByCategory extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            value: this.props.value,
             userId: '',
             view:"projects",
             config:{
@@ -31,17 +32,69 @@ class BillStatusByCategory extends Component {
         }
     }
 
+    componentWillReceiveProps(nextProps){
+        var self = this;
+        if(nextProps.value !== this.props.value){
+            this.setState({value:nextProps.value});
+            var payload ={categorydata : this.state.value}
+            API.fetchbillsData(payload)
+                .then(
+                    (response) =>{
+                        console.log("in bill status by category",response.data);
+
+                        var res = response.data.filter(function (el ) {
+                            return self.state.value.indexOf(el._id.category) >= 0;
+                        });
+                        // filter by category
+                        console.log(res);
+
+
+                        var authors = [];
+                        var counts= [];
+
+                        res.map((data) => {
+
+                            authors.push({name:data._id.status,y:data.count});
+
+                            //counts.push(data.count);
+                        })
+
+                        console.log(" *******authors1 : " + JSON.stringify(authors) );
+                        //console.log("count of passed",res.data[0].count)
+                        //console.log("counts : " + counts);
+
+                        self.setState({
+                            config:{
+                                chart: {
+                                    type: 'pie'
+                                },
+                                title: {
+                                    text: 'Bill Status for a particular Category'
+                                },
+                                /* HighchartsConfig */
+                                series: [{
+                                    name: "Status",
+                                    data: authors
+
+
+                                }]
+                            },
+                        });
+                    });
+        }
+    }
+
 
     componentDidMount(){
         var self = this;
-        var payload ={categorydata : "Health-care"}
+        var payload ={categorydata : this.state.value}
         API.fetchbillsData(payload)
             .then(
                 (response) =>{
                     console.log("in bill status by category",response.data);
 
                     var res = response.data.filter(function (el ) {
-                        return "Health-care".indexOf(el._id.category) >= 0;
+                        return self.state.value.indexOf(el._id.category) >= 0;
                     });
                     // filter by category
                     console.log(res);
