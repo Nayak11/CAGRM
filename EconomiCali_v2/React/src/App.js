f1af4fd935d0df908178b08afe5823a7c5d57302
Nayak11 <snsuhasnayak@gmail.com>
2018-05-09 12:27:32 -0700

    updated

--- EconomiCali_v2/React/src/App.js
+++ EconomiCali_v2/React/src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+
 import './App.css';
+
 import {BrowserRouter} from 'react-router-dom';
+
 import NewerHomePage from "./components/NewerHomePage";
+
 import HomePage from "./components/HomePage";
+
 import Navbarmain from "./components/Navbarmain";
+
 import LogoImage from "./components/LogoImage";
+
 import Signup from "./components/Signup";
 
 
